964c53d639d3885dadbe6cce2051584a9e5660cd
bhavinrasadiya <bhavinrasadiya@example.com>
2022-05-14 03:30:38 +0530

    Fix FAQ title-image and nav icon - added map

--- components/FAQPage/index.tsx
+++ components/FAQPage/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import * as MS from "../ContactPage/components/ContactForm/StyledComponents";
 import data from "./data";
 import SingleQuestion from "./question";
 
@@ -10,19 +11,24 @@ const FAQPage = () => {
   return (
     <main>
       <S.Container>
-        <S.MainTitle>questions and answers about login</S.MainTitle>
-        {data.length && (
-          <S.HeaderInfo>
-            {data.map(question => {
-              return (
-                <SingleQuestion
-                  key={question.id}
-                  {...question}
-                ></SingleQuestion>
-              );
-            })}
-          </S.HeaderInfo>
-        )}
+        <MS.ContactImgContainer>
+          <MS.ContactImg src="imgs/contact_img.png" alt="" />
+        </MS.ContactImgContainer>
+        <div>
+          <S.MainTitle>questions and answers about login</S.MainTitle>
+          {data.length && (
+            <S.HeaderInfo>
+              {data.map((question) => {
+                return (
+                  <SingleQuestion
+                    key={question.id}
+                    {...question}
+                  ></SingleQuestion>
+                );
+              })}
+            </S.HeaderInfo>
+          )}
+        </div>
       </S.Container>
     </main>
   );
