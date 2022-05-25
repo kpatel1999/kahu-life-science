d54ca7cf3564c3823a9b1109842953b5c413754f
bhavinrasadiya <bhavinrasadiya@example.com>
2022-05-26 00:39:35 +0530

    Added hero section in home page

--- components/HomePage/components/Hero/index.tsx
+++ components/HomePage/components/Hero/index.tsx
@@ -1,48 +1,33 @@
 import React from "react";
 
-// Next
-import Image from "next/image";
-import Link from "next/link";
-
-// Utils
-// import getImageUrl from "utils/getImageUrl";
-
 // Components
 import * as S from "./StyledComponents";
-import getImageUrl from "../../../../utils/getImageUrl";
-// import BlackButton from "components/shared/BlackButton";
+import Image from "../../../shared/NewImage";
 
 const Hero = () => {
   return (
     <S.Wrapper>
       <S.TitleContainer>
-        <S.Title>
-          The decentralized pooled betting/gaming metaverse. Play the games or
-          open your own rooms.
-        </S.Title>
-        <S.Description>
-          The games you know and love in a pooled betting, “no-house” setup for
-          true-probability based best returns. Sports betting, political races,
-          card games (poker, blackjack, etc),casino games (roulette, craps,
-          etc.), dice games and much more. <S.Span>All in one place.</S.Span>
-        </S.Description>
-        {/* <Link href="/games" passHref>
-          <a>
-            <S.ButtonWrapper>
-              <BlackButton>
-                Preview what<span>&apos;</span>s coming
-              </BlackButton>
-            </S.ButtonWrapper>
-          </a>
-        </Link> */}
+        <S.Title>Healthy Life!</S.Title>
+        <S.Title>&nbsp; &nbsp; Healthy Word!</S.Title>
+
+        <S.TitleImageContainer>
+          <Image
+            src={"/imgs/hero_3.svg"}
+            height={15}
+            width={100}
+            alt="site logo"
+            loading="lazy"
+          />
+        </S.TitleImageContainer>
       </S.TitleContainer>
       <S.ImageWrapper>
         <S.ImageContainer>
           <Image
-            src={getImageUrl("/imgs/homepageMainImage.svg", true)}
-            layout="fill"
+            src={"/imgs/hero_2.svg"}
+            height={20}
+            width={20}
             alt="site logo"
-            objectFit="contain"
             loading="lazy"
           />
         </S.ImageContainer>
